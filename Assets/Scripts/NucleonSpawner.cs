3a35de76a39d7225fef1d6f585186837f1da9589
cdk1220 <donkuruppu@Dons-MacBook-Pro.local>
2018-04-04 12:54:30 -0500

    Implemented spawning and making the spawned thing shoot to the center of the screen.

--- Assets/Scripts/NucleonSpawner.cs
+++ Assets/Scripts/NucleonSpawner.cs
@@ -7,4 +7,27 @@ public class NucleonSpawner : MonoBehaviour {
     public float spawnDistance;
 
     public Nucleon[] nucleonPrefabs;
+
+    float timeSinceLastSpawn;
+
+    void FixedUpdate() {
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (timeSinceLastSpawn >= timeBetweenSpawns) {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+    }
+
+    void SpawnNucleon() {
+
+        // Randomly select a prefab 
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+
+        // Instantitate it
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+
+        // Giving it a random position to start
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
 }
