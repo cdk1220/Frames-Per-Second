6abc2ad47a381729329d80c23e409922348d251c
cdk1220 <donkuruppu@Dons-MacBook-Pro.local>
2018-04-04 11:55:51 -0500

    Custom nuclear component was created.

---
+++ Assets/Scripts/Nucleon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/* A Rigidbody component will always be added to the gameobject which this script
+ * is attached to */
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour {
+    public float attractionForce;
+
+    private Rigidbody body;
+
+    void Awake() {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        
+        /* By inverting the position vector of the gameobject with respect to the origin
+         * and applying after desired scaling makes the object shoot to the center of the
+         * screen */
+        body.AddForce(transform.localPosition * -attractionForce);
+    }
+}
